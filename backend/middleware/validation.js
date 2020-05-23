283cdbefba6ceeaba4f829adb072bce22c2aa844
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-05-23 14:57:25 +0200

    commit

---
+++ backend/middleware/validation.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+module.exports = (req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return next();
+  }
+  try {
+    const token = req.headers.authorization.split(' ')[1]; 
+    if (!token) {
+      throw new Error('Authentication failed!');
+    }
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (err) {
+    console.error({msg: 'Authentication failed!'});
+    return next(error);
+  }
+};
