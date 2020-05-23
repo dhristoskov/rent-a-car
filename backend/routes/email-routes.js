3825a2425e50562809030b0faa122a4b609b96d3
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-05-23 14:51:23 +0200

    commit update route

--- backend/routes/email-routes.js
+++ backend/routes/email-routes.js
@@ -4,7 +4,9 @@ const { check } = require('express-validator');
 const emailController = require('../controllers/email-controllers');
 const router = express.Router();
 
+//Send Invoice and checkout
 router.post('/send/:id', emailController.sendInvoiceEmail);
+//Send reset password link by mail
 router.post('/reset',
     [
         check('email', 'Please provide an email').isEmail().not().isEmpty().isLength({ min: 6 }).normalizeEmail()
